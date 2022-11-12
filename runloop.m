4a61fb7005413de79e36c5b985f24a18ec4dd443
Eleanor Dicharry <deleanor440@gmail.com>
2022-11-12 12:29:20 -0500

    Stop runloop after a fixed number of fires.

--- runloop.m
+++ runloop.m
@@ -6,6 +6,9 @@ static void callback(CFRunLoopTimerRef timer, void *info) {
 	int *value = (int *)info;
 	*value += 1;
 	printf("%d\n", *value);
+	if (*value == 5) {
+		CFRunLoopStop(CFRunLoopGetCurrent());
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -16,9 +19,10 @@ int main(int argc, char* argv[]) {
 
         bzero(&timer_context, sizeof(timer_context));
         timer_context.info = &i;
-        timer = CFRunLoopTimerCreate(NULL, CFAbsoluteTimeGetCurrent(), 1, 0, 0, callback, &timer_context);
+        timer = CFRunLoopTimerCreate(NULL, CFAbsoluteTimeGetCurrent(), 0.5, 0, 0, callback, &timer_context);
         CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes);
         CFRunLoopRun();
 	}
+	printf("Exiting.\n");
 	return 0;
 }
